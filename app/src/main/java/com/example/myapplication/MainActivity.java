96efbc04d73f08c21f307a0f9a8e102546d5065b
Saidi Qemali <sajdiqemali@gmail.com>
2024-01-18 09:37:10 +0100

    MainActivity main variables added

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText inputPlayer1;
+    private EditText inputPlayer2;
+    private Button shakeButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        inputPlayer1 = findViewById(R.id.player1);
+        inputPlayer2 = findViewById(R.id.player2);
+        shakeButton = findViewById(R.id.shakeButton);
+
     }
 }
