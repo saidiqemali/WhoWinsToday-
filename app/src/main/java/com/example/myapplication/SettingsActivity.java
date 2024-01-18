21a271fa8befe07b97d5d294bc360af201f90925
Saidi Qemali <sajdiqemali@gmail.com>
2024-01-18 11:22:18 +0100

    SettingsActivity read names from SharedPreferences and save points

--- app/src/main/java/com/example/myapplication/SettingsActivity.java
+++ app/src/main/java/com/example/myapplication/SettingsActivity.java
@@ -1,4 +1,61 @@
 package com.example.myapplication;
 
-public class SettingsActivity {
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    private TextView inputPlayer1;
+    private TextView inputPlayer2;
+    private TextView player1points;
+    private TextView player2points;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        inputPlayer1 = findViewById(R.id.player1Game);
+        inputPlayer2 = findViewById(R.id.player2Game);
+        player1points = findViewById(R.id.player1points);
+        player2points = findViewById(R.id.player2points);
+
+
+        inputPlayer1.setText(getSpieler1());
+        inputPlayer2.setText(getSpieler2());
+    }
+
+
+    public String getSpieler1() {
+        SharedPreferences sharedPreferences = getSharedPreferences("Speicher", MODE_PRIVATE);
+        String player1 = sharedPreferences.getString("spieler1", null);
+        return player1;
+    }
+
+
+    public String getSpieler2() {
+        SharedPreferences sharedPreferences = getSharedPreferences("Speicher", MODE_PRIVATE);
+        String player2 = sharedPreferences.getString("spieler2", null);
+        return player2;
+    }
+
+    public void savePlayer1Points(int amount) {
+        SharedPreferences sharedPreferences = getSharedPreferences("Speicher", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putInt("player1points", amount);
+        editor.apply();
+    }
+
+    public void savePlayer2Points(int amount) {
+        SharedPreferences sharedPreferences = getSharedPreferences("Speicher", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putInt("player2points", amount);
+        editor.apply();
+    }
 }
