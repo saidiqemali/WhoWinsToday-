78116266482de48dbb9ed1e22c65a8385c69fb51
Saidi Qemali <sajdiqemali@gmail.com>
2024-01-19 13:08:17 +0100

    Added logic to replay button

--- app/src/main/java/com/example/myapplication/FinalActivity.java
+++ app/src/main/java/com/example/myapplication/FinalActivity.java
@@ -1,10 +1,15 @@
 package com.example.myapplication;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class FinalActivity extends AppCompatActivity {
+
+    private Button replayButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +19,15 @@ public class FinalActivity extends AppCompatActivity {
         String winner = getIntent().getStringExtra("Winner-Loser");
         TextView winnerTextView = findViewById(R.id.winnerTextView);
         winnerTextView.setText("Winner: " + winner);
+
+        replayButton = findViewById(R.id.replayButton);
+        replayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FinalActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
